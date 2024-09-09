1281f2f2f6ad24ad915362467a1d4e5dc3a41805
yuduxing <avalonxy@gmail.com>
2024-09-09 18:47:53 +0800

    add taro ui

--- config/index.js
+++ config/index.js
@@ -51,6 +51,7 @@ export default defineConfig(async (merge, { command, mode }) => {
     h5: {
       publicPath: '/',
       staticDirectory: 'static',
+      esnextModules: ["taro-ui"],
       output: {
         filename: 'js/[name].[hash:8].js',
         chunkFilename: 'js/[name].[chunkhash:8].js'
